bd80d126e4d82a7cd0b2cc2ba33d282dd29ee69a
Levente Krotos <krotos.levi@gmail.com>
2023-03-03 15:01:52 +0100

    indent fix

--- src/App.tsx
+++ src/App.tsx
@@ -13,44 +13,44 @@ import Projects from './Components/Projects/Projects';
 import Resume from './Components/Resume/Resume';
 
 const App: React.FC = () => (
-    <main className={cn(
-      "desktop:px-10 mobile:px-2 flex bg-gradient-to-b from-slate-400 to-gray-700 min-h-screen h-fit"
+  <main className={cn(
+    "desktop:px-10 mobile:px-2 flex bg-gradient-to-b from-slate-400 to-gray-700 min-h-screen h-fit"
+  )}>
+    <section className={cn(
+      "flex flex-col flex-1 w-full"
     )}>
-      <section className={cn(
-        "flex flex-col flex-1 w-full"
-      )}>
-        <NavBar />
-        <Routes>
-          <Route
-            path="/home"
-            element={(
-              <Navigate
-                to="/"
-                replace
-              />
-            )}
-          />
-          <Route 
+      <NavBar />
+      <Routes>
+        <Route
+          path="/home"
+          element={(
+            <Navigate
+              to="/"
+              replace
+            />
+          )}
+        />
+        <Route 
           path="/"
           element={
             <Introduction />
           }
-          />
-          <Route 
+        />
+        <Route 
           path="/projects"
           element={
             <Projects />
           }
-          />
-          <Route 
+        />
+        <Route 
           path="/resume"
           element={
             <Resume />
           }
-          />
-        </Routes>
-      </section>
-    </main>
-  );
+        />
+      </Routes>
+    </section>
+  </main>
+);
 
 export default App;
--- src/Components/ContactPlatforms/ContactPlatforms.tsx
+++ src/Components/ContactPlatforms/ContactPlatforms.tsx
@@ -3,19 +3,19 @@ import { NavLink } from 'react-router-dom';
 import PlatformsIcons from './ContactPlatformTypes';
 
 const ContactPlatforms: React.FC = () => (
-      <div className="text-5xl flex justify-center gap-20 py-3 text-gray-300">
-        {PlatformsIcons.map(platform => (
-          <label htmlFor={platform.id} className="group">
-            <div id={platform.id} className="group-hover:animate-bounce">
-            <NavLink
-              to={platform.Path}
-            >
-              {platform.icon}
-            </NavLink>
-            </div>
-          </label>
-        ))}
-      </div>
+  <div className="text-5xl flex justify-center gap-20 py-3 text-gray-300">
+    {PlatformsIcons.map(platform => (
+      <label htmlFor={platform.id} className="group">
+        <div id={platform.id} className="group-hover:animate-bounce">
+          <NavLink
+            to={platform.Path}
+          >
+            {platform.icon}
+          </NavLink>
+        </div>
+      </label>
+    ))}
+  </div>
 );
 
 export default ContactPlatforms;
