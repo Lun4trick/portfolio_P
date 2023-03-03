1ce3a680732a91a43801b12b36d417ff0aaea4f9
Levente Krotos <krotos.levi@gmail.com>
2023-03-03 08:39:48 +0100

    fixed deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import cn from 'classnames';
 import {
   Navigate,
   Route,
-  Routes,
-  useLocation
+  Routes
 //   Navigate,
 } from 'react-router-dom';
 import './App.css';
@@ -13,32 +12,7 @@ import NavBar from './Components/NavBar/NavBar';
 import Projects from './Components/Projects/Projects';
 import Resume from './Components/Resume/Resume';
 
-const App: React.FC = () => {
-  const [scrollBarVisible, setScrollBarVisible] = useState(false);
-  const location = useLocation();
-
-  function handleScroll() {
-    const isScrollable = window.scrollY > 0;
-    setScrollBarVisible(isScrollable);
-  }
-
-  useEffect(() => {
-    window.addEventListener('resize', handleScroll);
-    window.addEventListener('scroll', handleScroll);
-
-    return () => {
-      window.removeEventListener('resize', handleScroll);
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
-
-  useEffect(handleScroll, [location])
-
-  console.log(scrollBarVisible)
-
-
-  
-  return (
+const App: React.FC = () => (
     <main className={cn(
       "desktop:px-10 mobile:px-2 flex bg-gradient-to-b from-slate-400 to-gray-700 min-h-screen h-fit"
     )}>
@@ -78,6 +52,5 @@ const App: React.FC = () => {
       </section>
     </main>
   );
-}
 
 export default App;
